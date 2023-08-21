a384c01c008ecf6102a21743d92fae776dcd06d1
KallDrexx <me@mshapiro.net>
2023-08-20 22:49:26 -0300

    Revert color bit packing, as it wasn't saving colors as expected.  Not sure why.

--- microgpu-common/color.h
+++ microgpu-common/color.h
@@ -9,11 +9,7 @@ typedef enum {
 
 #ifdef MGPU_COLOR_MODE_USE_RGB565
 
-typedef struct {
-    uint8_t red : 5;
-    uint8_t green: 6;
-    uint8_t blue: 5;
-} Mgpu_Color;
+typedef uint16_t Mgpu_Color;
 
 /*
  * Creates a color based on red, green, and blue values already converted
--- microgpu-common/color_rgb565.c
+++ microgpu-common/color_rgb565.c
@@ -4,22 +4,6 @@ Mgpu_ColorMode mgpu_color_get_mode(void) {
     return MgpuColorMode_Rgb565;
 }
 
-Mgpu_Color mgpu_color_from_rgb565(uint8_t red, uint8_t green, uint8_t blue) {
-    Mgpu_Color color = {
-            .red = red,
-            .green = green,
-            .blue = blue,
-    };
-
-    return color;
-}
-
-void mgpu_color_get_rgb565(Mgpu_Color color, uint8_t *red, uint8_t *green, uint8_t *blue) {
-    *red = color.red;
-    *green = color.green;
-    *blue = color.blue;
-}
-
 Mgpu_Color mgpu_color_from_rgb888(uint8_t red, uint8_t green, uint8_t blue) {
     // Convert values from 24bit space values to 16bit space values, before
     // packing them into a 16bit rgb565 number
@@ -27,15 +11,13 @@ Mgpu_Color mgpu_color_from_rgb888(uint8_t red, uint8_t green, uint8_t blue) {
     green = green / 4;
     blue = blue / 8;
 
-    return mgpu_color_from_rgb565(red, green, blue);
+    return ((uint16_t)red << 11) | ((uint16_t)green << 5) | blue;
 }
 
 void mgpu_color_get_rgb888(Mgpu_Color color, uint8_t *red, uint8_t *green, uint8_t *blue) {
-    mgpu_color_get_rgb565(color, red, green, blue);
-
-    uint16_t tempRed = (uint16_t)(*red) * 8;
-    uint16_t tempGreen = (uint16_t)(*green) * 4;
-    uint16_t tempBlue = (uint16_t)(*green) * 8;
+    uint16_t tempRed = (color >> 11) * 8;
+    uint16_t tempGreen = ((color & 0x0730) >> 5) * 4;
+    uint16_t tempBlue = (color & 0x1F) * 8;
 
     if (tempRed > 255) {
         tempRed = 255;
@@ -52,4 +34,14 @@ void mgpu_color_get_rgb888(Mgpu_Color color, uint8_t *red, uint8_t *green, uint8
     *red = tempRed;
     *green = tempGreen;
     *blue = tempBlue;
+}
+
+Mgpu_Color mgpu_color_from_rgb565(uint8_t red, uint8_t green, uint8_t blue) {
+    return ((uint16_t)red << 11) | ((uint16_t)green << 5) | blue;
+}
+
+void mgpu_color_get_rgb565(Mgpu_Color color, uint8_t *red, uint8_t *green, uint8_t *blue) {
+    *red = (color >> 11);
+    *green = ((color & 0x0730) >> 5);
+    *blue = color & 0x001f;
 }
