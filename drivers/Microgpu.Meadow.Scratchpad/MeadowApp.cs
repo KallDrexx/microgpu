b618fad78fa22c7bb5089669af440dabb645afc4
KallDrexx <me@mshapiro.net>
2023-10-13 16:44:18 -0400

    Changed meadow sample to work for the project lab

--- drivers/Microgpu.Meadow.Scratchpad/MeadowApp.cs
+++ drivers/Microgpu.Meadow.Scratchpad/MeadowApp.cs
@@ -2,39 +2,66 @@
 using System.Threading.Tasks;
 using Meadow;
 using Meadow.Devices;
+using Meadow.Foundation.ICs.IOExpanders;
 using Meadow.Hardware;
 using Meadow.Units;
 using Microgpu.Common;
 using Microgpu.Common.Comms;
 using Microgpu.Sample.Common;
 
-namespace Microgpu.Meadow.Scratchpad;
-
-public class MeadowApp : App<F7FeatherV2>
+namespace Microgpu.Meadow.Scratchpad
 {
-    private Gpu _gpu = null!;
-
-    public override async Task Initialize()
+    public class MeadowApp : App<F7FeatherV2>
     {
-        var reset = Device.CreateDigitalOutputPort(Device.Pins.D02, true);
-        var handshake = Device.CreateDigitalInputPort(Device.Pins.D03);
-        var chipSelect = Device.CreateDigitalOutputPort(Device.Pins.D04, true);
+        private Gpu _gpu = null!;
+        
+        public override async Task Initialize()
+        {
+            var mcpIn = Device.CreateDigitalInterruptPort(
+                Device.Pins.D10,
+                InterruptMode.EdgeRising,
+                ResistorMode.Disabled);
+            var mcp = new Mcp23008(Device.CreateI2cBus(), 0x21, mcpIn);
+            
+            var reset = Device.CreateDigitalOutputPort(Device.Pins.D03, true);
+            var handshake = mcp.CreateDigitalInputPort(mcp.Pins.GP6, ResistorMode.Disabled);
+            // var handshake = mcp.CreateDigitalInterruptPort(mcp.Pins.GP6);
+            var chipSelect = mcp.CreateDigitalOutputPort(mcp.Pins.GP5, true);
+            
+            // handshake.Changed += (sender, args) =>
+            // {
+            //     Console.WriteLine($"Handshake changed: {args.New}");
+            // };
 
-        var config = new SpiClockConfiguration(
-            new Frequency(10, Frequency.UnitType.Megahertz),
-            SpiClockConfiguration.Mode.Mode0);
 
-        var spiBus = Device.CreateSpiBus(Device.Pins.SCK, Device.Pins.COPI, Device.Pins.CIPO, config);
+            // var projectLab = ProjectLab.Create();
+            // var handshake = projectLab.MikroBus1.Pins.INT.CreateDigitalInputPort();
+            // var reset = projectLab.MikroBus1.Pins.RST.CreateDigitalOutputPort(true);
+            // var chipSelect = projectLab.MikroBus1.Pins.CS.CreateDigitalOutputPort(true);
+            
+            
+            var config = new SpiClockConfiguration(
+                new Frequency(10, Frequency.UnitType.Megahertz),
+                SpiClockConfiguration.Mode.Mode0);
+            
+            // var spiBus = projectLab.SpiBus;
+            // spiBus.Configuration.Speed = new Frequency(10, Frequency.UnitType.Megahertz);
+            var spiBus = Device.CreateSpiBus(Device.Pins.SCK, Device.Pins.COPI, Device.Pins.CIPO, config);
+            
+            Console.WriteLine("Initializing GPU");
+            var gpuCommunication = new MeadowSpiGpuCommunication(spiBus, handshake, reset, chipSelect);
+            _gpu = await Gpu.CreateAsync(gpuCommunication);
+            await _gpu.InitializeAsync(1);
+        }
 
-        Console.WriteLine("Initializing GPU");
-        var gpuCommunication = new MeadowSpiGpuCommunication(spiBus, handshake, reset, chipSelect);
-        _gpu = await Gpu.CreateAsync(gpuCommunication);
-        await _gpu.InitializeAsync(1);
-    }
-
-    public override async Task Run()
-    {
-        var sampleRunner = new SampleRunner(_gpu, TimeSpan.FromMilliseconds(0));
-        await sampleRunner.Run();
+        public override async Task Run()
+        {
+            var sampleRunner = new SampleRunner(_gpu, TimeSpan.FromMilliseconds(0));
+            await sampleRunner.Run();
+            // while (true)
+            // {
+            //     await Task.Delay(100);
+            // }
+        }
     }
 }
